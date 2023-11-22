7e67b7191cbb3a79f515843a5cf577120700c0cd
Md Shakil monsi <samdshakilofficial@gmail.com>
2023-11-22 18:29:30 +0600

    add footer  besice secyion setup

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,117 +1,151 @@
 import React from 'react';
-import { FaFacebook, FaInstagram, FaPinterest, FaTwitter } from 'react-icons/fa';
+// import css link 
 import "./footer.css"
+// import img and logo link 
+import closslogo from"../../../assets/footerAll_icon/close.svg"
+//Download Our App import
+
+import apple from"../../../assets/footerAll_icon/apple.svg"
+import playstore from"../../../assets/footerAll_icon/playstore.svg"
+// import Follow US logo
+import facebook from"../../../assets/footerAll_icon/facebook.png"
+import instagram from"../../../assets/footerAll_icon/instagram.png"
+import whatsapp from"../../../assets/footerAll_icon/whatsapp.png"
+import email from"../../../assets/footerAll_icon/email.png"
+
+
+
 import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
-    <div className='mt-3'>
-      <footer className="footer " style={{
+    <div className='mt-3' style={{marginLeft:0}}>
+
+      <footer className="footer  " style={{
         backgroundColor: "#EA4D00"
 
       }}>
        
           <div className=" row  pading_option_setup " >
             <div className="d-flex grid_footer_item">
-              <div className='j'>
-                <h6>Company Information</h6>
+              <div>
+               
                 <ul>
-                  <Link to="/">users add</Link>
+                <h6 className='footer_title_text_style'>Company Information</h6>
+                  <Link  to="/"   className='style_to_link_text'>users add</Link>
                   <br></br>
-                  <Link to="/">contact your</Link>
+                  <Link  to="/"   className='style_to_link_text'>contact your</Link>
                   <br></br>
-                  <Link to="/">order add</Link>
+                  <Link  to="/"   className='style_to_link_text'>order add</Link>
                   <br></br>
-                  <Link to="/">newProduct add</Link>
+                  <Link  to="/"   className='style_to_link_text'>newProduct add</Link>
                   <br></br>
-                  <Link to="/">oldProduct add</Link>
+                  <Link  to="/"   className='style_to_link_text'>oldProduct add</Link>
+                </ul>
+              </div>
+              <div className='style_to_link_text'>
+               
+                <ul>
+                <h6  className='footer_title_text_style'>Brands</h6>
+                  <Link  to="/"   className='style_to_link_text'>users add</Link>
+                  <br></br>
+                  <Link  to="/"   className='style_to_link_text'>contact your</Link>
+                  <br></br>
+                  <Link  to="/"   className='style_to_link_text'>order add</Link>
+                  <br></br>
+                  <Link  to="/"   className='style_to_link_text'>newProduct add</Link>
+                  <br></br>
+                  <Link  to="/"   className='style_to_link_text'>oldProduct add</Link>
                 </ul>
               </div>
               <div>
-                <h6>Useful Links</h6>
+              
                 <ul>
-                  <Link to="/">users add</Link>
+                  <h6 className='footer_title_text_style'>Categories</h6>
+                  <Link  to="/"   className='style_to_link_text'>users add</Link>
                   <br></br>
-                  <Link to="/">contact your</Link>
+                  <Link  to="/"   className='style_to_link_text'>contact your</Link>
                   <br></br>
-                  <Link to="/">order add</Link>
+                  <Link  to="/"   className='style_to_link_text'>order add</Link>
                   <br></br>
-                  <Link to="/">newProduct add</Link>
+                  <Link  to="/"   className='style_to_link_text'>newProduct add</Link>
                   <br></br>
-                  <Link to="/">oldProduct add</Link>
+                  <Link  to="/"   className='style_to_link_text'>oldProduct add</Link>
                 </ul>
               </div>
               <div>
-                <h6>Useful Links</h6>
-                <ul>
-                  <Link to="/">users add</Link>
+                <ul> 
+                  <h6 className='footer_title_text_style'>Market & Sell</h6>
+                  <Link  to="/"   className='style_to_link_text'>users add</Link>
                   <br></br>
-                  <Link to="/">contact your</Link>
+                  <Link  to="/"   className='style_to_link_text'>contact your</Link>
                   <br></br>
-                  <Link to="/">order add</Link>
+                  <Link  to="/"   className='style_to_link_text'>order add</Link>
                   <br></br>
-                  <Link to="/">newProduct add</Link>
+                  <Link  to="/"   className='style_to_link_text'>newProduct add</Link>
                   <br></br>
-                  <Link to="/">oldProduct add</Link>
+                  <Link  to="/"   className='style_to_link_text'>oldProduct add</Link>
                 </ul>
               </div>
               <div>
-                <h6>Useful Links</h6>
                 <ul>
-                  <Link to="/">users add</Link>
+                  <h6 className='footer_title_text_style'>Customer Service</h6>
+                  <Link  to="/"   className='style_to_link_text'>How to Order</Link>
                   <br></br>
-                  <Link to="/">contact your</Link>
+                  <Link  to="/"   className='style_to_link_text'>Track Your Order</Link>
                   <br></br>
-                  <Link to="/">order add</Link>
+                  <Link  to="/"   className='style_to_link_text'>FAQ</Link>
                   <br></br>
-                  <Link to="/">newProduct add</Link>
+                  <Link  to="/"   className='style_to_link_text'>Contact us</Link>
                   <br></br>
-                  <Link to="/">oldProduct add</Link>
+                  <Link  to="/"   className='style_to_link_text'>Return / Refund Policy</Link>
+                  <Link  to="/"   className='style_to_link_text'>Terms & Conditions</Link>
+                  <Link  to="/"   className='style_to_link_text'>Privacy Policy</Link>
+                  <Link  to="/"   className='style_to_link_text'></Link>
                 </ul>
               </div>
               <div>
-                <h6>Useful Links</h6>
+            
                 <ul>
-                  <Link to="/">users add</Link>
+                  <h6 className='footer_title_text_style'>Follow US</h6>
                   <br></br>
-                  <Link to="/">contact your</Link>
+                  <div className='d-flex gap-3  faceBook_logo_setup' style={{color:"#F1F1F1"}}><img src={facebook} alt=''></img>  <Link  to="/"   className='style_to_link_text'>facebook</Link>   </div>
+                  
                   <br></br>
-                  <Link to="/">order add</Link>
+                  <div className='d-flex gap-3  Instagram_logo_setup' style={{color:"#F1F1F1"}}><img src={instagram} alt=''></img>  <Link  to="/"   className='style_to_link_text'>Instagram</Link>   </div>
+ 
                   <br></br>
-                  <Link to="/">newProduct add</Link>
+                  <div className='d-flex gap-3  faceBook_logo_setup' style={{color:"#F1F1F1"}}><img src={whatsapp} alt=''></img>  <Link  to="/"   className='style_to_link_text'>WhatsApp</Link>   </div>
+ 
                   <br></br>
-                  <Link to="/">oldProduct add</Link>
+                  <div className='d-flex gap-3  email_logo_setup' style={{color:"#F1F1F1"}}><img src={email} alt=''></img>  <Link  to="/"   className='style_to_link_text'>Email</Link>   </div>
+ 
+                  
+                
+               
                 </ul>
               </div>
+
+{/* Download Our App   and playstore section  */}
+
+
               <div>
-                <h6>Useful Links</h6>
+               
                 <ul>
-                  <Link to="/">usersadd</Link>
-                  <br></br>
-                  <Link to="/">contactyour</Link>
-                  <br></br>
-                  <Link to="/">orderadd</Link>
-                  <br></br>
-                  <Link to="/">newProductadd</Link>
-                  <br></br>
-                  <Link to="/">oldProductadd</Link>
+                  <h6 className='footer_title_text_style'>Download Our App</h6>
+                  <p  to="/"   className='style_to_link_text'>Get the Cart24 mobile   app for FREE from here</p>
+           
+               
+               
+                  <img className='apple_playstore_logo' src={apple} alt='' style={{marginLeft:50}}></img>
+                <br></br>
+                  <img  className='apple_playstore_logo' src={playstore} alt='' style={{marginLeft:50}}></img>
+
+      
                 </ul>
               </div>
+             
               <div>
-                <h6>Useful Links</h6>
-                <ul>
-                  <Link to="/">users add</Link>
-                  <br></br>
-                  <Link to="/">contact your</Link>
-                  <br></br>
-                  <Link to="/">order add</Link>
-                  <br></br>
-                  <Link to="/">newProduct add</Link>
-                  <br></br>
-                  <Link to="/">oldProduct add</Link>
-                </ul>
-              </div>
-              <div>
-              +
+              <img src={closslogo} alt=''></img>
               </div>
 
           </div>
